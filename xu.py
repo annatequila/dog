5e170ed80d876a37427c55c46908ea35f9153c82
annatequila <664801256@qq.com>
2026-07-02 14:12:49 +0800

    类继承‘

--- xu.py
+++ xu.py
@@ -1,24 +1,24 @@
 import numpy as np
 
-class Dog:
-    species = "Felix"  # 类属性（共享）
-    def __init__(self, name, age):
-        self.name = name        # 实例属性
-        self.age = age
+# class Dog:
+#     species = "Felix"  # 类属性（共享）
+#     def __init__(self, name, age):
+#         self.name = name        # 实例属性
+#         self.age = age
 
-    def bark(self):
-        print(f"{self.species}")
-        print(f"{self.name}：汪汪！")
-    def sit(self):
-        print(f"{self.name}坐下了。")
+#     def bark(self):
+#         print(f"{self.species}")
+#         print(f"{self.name}：汪汪！")
+#     def sit(self):
+#         print(f"{self.name}坐下了。")
 
-class Cat:
-    species = "Felis"  # 类属性（共享）
-    def __init__(self, name):
-        self.name = name  # 实例属性（每个对象不同）
-    def meow(self):
-        print(f"{self.species}")
-        print(f"{self.name}：喵喵！")
+# class Cat:
+#     species = "Felis"  # 类属性（共享）
+#     def __init__(self, name):
+#         self.name = name  # 实例属性（每个对象不同）
+#     def meow(self):
+#         print(f"{self.species}")
+#         print(f"{self.name}：喵喵！")
 
 
 class Animal:
@@ -37,6 +37,22 @@ for a in (Dog(), Cat()):
     print(a.speak())  # 多态：相同接口，不同实现
 
 
+class fath:
+    def __init__(self, name):
+        self.name = name
+        self.age = self.name + '的年龄'
+        print("父类构造函数被调用")
+    def show(self):
+        print("掉了")
+
+class son(fath):
+    def __init__(self, name):
+        super().__init__(name)  # 调用父类构造函数
+        print("儿子")
+        print(f"{self.age}")
+
+ff = son('haizi')
+ff.show()
 # f = Cat("小hon") 
 # f.meow()
 
