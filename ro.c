fdac210725d1455eb4e806ecfacbb5e21c2b8c02
annatequila <664801256@qq.com>
2026-07-02 08:26:34 +0800

    c偶见

---
+++ ro.c
@@ -0,0 +1,15 @@
+/***********************************************************
+  > File Name: ro.c
+  > Author: https://www.baidu.com/
+  > Mail: 1xxxxxx@qq.com
+  > Created Time: 2026-07-02 08:25:57
+ *******************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char *argv[]) {
+    printf("hello...\n");
+    return 0;
+}
+
