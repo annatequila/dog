f3a9ca9c6767b5867fe1dc177e26b86b3d17e7fd
annatequila <664801256@qq.com>
2026-07-10 09:18:03 +0800

    C++学习

---
+++ cir.h
@@ -0,0 +1,33 @@
+/***********************************************************
+  > File Name: cir.h
+  > Author: https://www.baidu.com/
+  > Mail: 1xxxxxx@qq.com
+  > Created Time: 2026-07-10 08:31:21
+ *******************************************************/
+
+
+
+#ifndef CIR_H
+
+#define CIR_H
+
+namespace Cir{
+
+const double PI = 3.141592653;
+
+double areaOfCircle(double radius){
+
+return PI*radius*radius;
+
+}
+
+double lenthOfCircle(double radius){
+
+return 2*PI*radius;
+
+}
+
+}
+
+#endif // CIR_H 
+
--- tx.cpp
+++ tx.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <stdio.h> 
+#include <locale>
+#include <climits>
+//  ****************************
+#include "cir.h"
+
+
 using namespace std;
 
 int main() {
+
+    cout << "hollow" << endl;
+
+    double radius = 5;
+
+
+    printf("半径为%.2f的圆，周长为%.2f,面积为%.2f\n",radius,Cir::lenthOfCircle(radius), 
+    Cir::areaOfCircle(radius));
+
+    cout << "半径为" << radius << "的圆," "周长为"<< Cir::lenthOfCircle(radius)<< ",面积为" << Cir::areaOfCircle(radius) << endl;
+    std::clog << "Logging: user entered a number." << std::endl; 
+
     vector<int> nums = {1, 2, 3};
     for (int num : nums) {
         cout << "Number: " << num << endl;  // 断点位置
     }
+
+    std::cout << "The range of int is from " << INT_MIN << " to " << INT_MAX <<
+
+    std::endl;
+
+    std::cout << "The maximum value of unsigned int is " << UINT_MAX <<
+
+    std::endl;
+
+    std::cout << "The range of long long is from " << LLONG_MIN << " to " << LLONG_MAX << std::endl; 
+
+   // int x=0;
+   // std::cin >> x;
+   // std::cout << x << endl;
+
     return 0;
 }
